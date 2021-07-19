a93241dfe33f0d3af3842ac54dad5fb847a7ce8c
SalamH <salam94tm@gmail.com>
2021-07-19 16:44:51 -0400

    initial commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello git world");
+
+
+        // We have added our GitHub account under inteliJ
+        // make sure to click apply and click on to save
     }
 }
 
